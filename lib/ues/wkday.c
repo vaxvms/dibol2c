b9817bdfc530889eb17652703bd068e0f80174e9
Nicolas DUPEUX <nicolas@dupeux.net>
2009-12-09 22:12:23 +0100

    Add WKDAY function to UES. Provided by Anu

---
+++ lib/ues/wkday.c
@@ -0,0 +1,30 @@
+/**************************************************************************
+ * wkday.c from libdibolues
+ *
+ * (c) 2009 Anu				info@aksuda.com
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ **************************************************************************/
+
+#include "dibol.h"
+#include <time.h>
+
+void pWKDAY(variable* afield)
+{
+	time_t curtime;
+	char* str=(char*)malloc(10*sizeof(char));
+	int i;
+	curtime=time(NULL);
+	strftime(str,10,"%A",localtime(&curtime));
+	memcpy(getdata(afield),str,getsize(afield));
+	free(str);
+}
+ 
